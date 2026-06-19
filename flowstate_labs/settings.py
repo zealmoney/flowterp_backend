f10e30be71cb19861310206c6b966685ab4a59d1
zealmoney <chatwithzeal@gmail.com>
2026-06-19 02:53:44 -0500

    Force production database URL config

--- flowstate_labs/settings.py
+++ flowstate_labs/settings.py
@@ -113,12 +113,12 @@ if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
     DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
             conn_max_age=0,
             ssl_require=True,
         )
-    }
+}
 else:
     DATABASES = {
         "default": {
